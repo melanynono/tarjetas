483e9516a71858377b9ec28c35c2716e48c66560
MiryamElizabeth2006 <miryamelizabeth367@gmail.com>
2026-07-10 16:29:52 -0500

    Agregar generación de tarjetas con desde, hasta y salto

--- tarjetas.js
+++ tarjetas.js
@@ -1,8 +1,15 @@
-function crearTarjetas(){
+function crearTarjetas() {
+
+    let desde = parseInt(document.getElementById("txtDesde").value);
+    let hasta = parseInt(document.getElementById("txtHasta").value);
+    let salto = parseInt(document.getElementById("txtSalto").value);
+
     let contenido = "";
-    let divTarjetas = document.getElementById("divTarjetas")
-    for(let i = 1; i<=5; i++){
-        contenido= contenido + "<div class ='item'>" + i + "</div>"
-        divTarjetas.innerHTML = contenido;
+    let divTarjetas = document.getElementById("divTarjetas");
+
+    for (let i = desde; i <= hasta; i = i + salto) {
+        contenido += "<div class='item'>" + i + "</div>";
     }
+
+    divTarjetas.innerHTML = contenido;
 }
